6d71aa210ac520be626b0c0b533811a4468a9f74
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 21:47:14 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -18,7 +18,8 @@ class Person
     candy_lover
   end
 
-  def ballin_candy_lover?
+  def ballin_candy_lover?(likes_candy)
+    likes_candy = candy_lover
     if likes_candy
       "Aw sweet, this cat loves them some candy!"
     else
@@ -28,4 +29,4 @@ class Person
     end
   end
 end
-ballin_candy_lover
+ballin_candy_lover(likes_candy)
