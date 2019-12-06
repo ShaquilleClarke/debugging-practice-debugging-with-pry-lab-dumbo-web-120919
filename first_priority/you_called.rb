e00bd9b3299dfd57a77cfdc511eeb6d5a1d9e77a
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 21:26:26 +0000

    Automatically backed up by Learn

--- first_priority/you_called.rb
+++ first_priority/you_called.rb
@@ -1,7 +1,9 @@
 
 
-super_mario_party
 
-def super_mario_party
+
+def super_mario_party(where_is_princess)
   "Sorry, your princess is in another castle"
 end
+
+super_mario_party(where_is_princess)
